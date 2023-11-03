98e068fe3e7589219a3c117f9e565ecb4320280c
The_Scientist777 <pmrojas195@gmail.com>
2023-11-03 12:30:33 -0500

    Starting files

---
+++ functions.h
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// a.1 Create your struct here.
+
+
+// a.2  Create your global variable here.
+
+
+
+// b.1 Declare your fill_inventory function here.
+
+
+// b.2 Declare your print_inventory function here.
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "functions.h"
+
+int main() {
+  // a.3 Create your car array here.
+
+  // a.4 Create your current variable here.
+
+  cout << "Welcome to Car Inventory!" << endl;
+  cout << "You have " << total << " car spaces available." << endl;
+
+  // b.3 Call your functions here.
+  
+
+  return 0;
+}
